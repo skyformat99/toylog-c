e4f02baf7abaed5d99583084ea6690bfdc6e9920
fengbohello <fengbohello@foxmail.com>
2017-01-02 07:50:36 -0500

    move code about file from toylog-write.c to toylog-file.c

--- src/toylog-file.h
+++ src/toylog-file.h
@@ -4,6 +4,10 @@
 #include "toytypes.h"
 
 int get_filename(char *szfilename, int len, LogOutput * output, int index);
+int toylog_write_file(LogOutput * output, int priority, const char * fmt, va_list arg_list);
+int toylog_file_full(LogOutput * output);
+int toylog_move_file(LogOutput * output, int index);
+int toylog_adjust_file(LogOutput * output);
 
 #endif
 
